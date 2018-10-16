dfc753ca74db74e6c5b8a6bbf48da181cd512d79
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-10-16 10:38:59 -0700

    update new and edit views

--- models/budgets.js
+++ models/budgets.js
@@ -1,13 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const BudgetSchema = new Schema({
     createdAt: Date,
     updatedAt: Date,
-    name: String,
-    amount:  String,
-    category: String
+    housingName: String,
+    housingAmount: String,
+    housingCategory: String,
+    foodName: String,
+    foodAmount: String,
+    foodCategory: String,
+    transportationName: String,
+    transportationAmount: String,
+    transportationCategory: String,
+    insuranceName: String,
+    insuranceAmount: String,
+    insuranceCategory: String,
+    clothingName: String,
+    clothingAmount: String,
+    clothingCategory: String,
+    entertainmentName: String,
+    entertainmentAmount: String,
+    entertainmentCategory: String,
+    savingName: String,
+    savingAmount: String,
+    savingCategory: String
 });
 
 BudgetSchema.pre("save", function(next) {
