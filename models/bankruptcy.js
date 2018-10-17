7b86cec6ce4e69330433a4e2143863195b5f080c
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-10-17 09:49:46 -0700

    update bankruptcy

---
+++ models/bankruptcy.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BankruptcySchema = new Schema({
+    housingName: { type: String, default: "housing" },
+    housingAmount: { type: String, default: 0 },
+    housingCategory: { type: String, default: "None" },
+    foodName: { type: String, default: "food" },
+    foodAmount: { type: String, default: 0 },
+    foodCategory: { type: String, default: "None" },
+    transportationName: { type: String, default: "transportation" },
+    transportationAmount: { type: Number, default: 0 },
+    transportationCategory: { type: String, default: "None" },
+    insuranceName: { type: String, default: "insurance" },
+    insuranceAmount: { type: Number, default: 0 },
+    insuranceCategory: { type: String, default: "None" },
+    clothingName: { type: String, default: "clothing" },
+    clothingAmount: { type: Number, default: 0 },
+    clothingCategory: { type: String, default: "None" },
+    entertainmentName: { type: String, default: "entertainment" },
+    entertainmentAmount: { type: Number, default: 0 },
+    entertainmentCategory: { type: String, default: "None" },
+    savingName: { type: String, default: "saving" },
+    savingAmount: { type: Number, default: 0 },
+    savingCategory: { type: String, default: "None" }
+});
+
+module.exports = mongoose.model("Bankruptcy", BankruptcySchema);
