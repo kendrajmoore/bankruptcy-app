41c99726e6c0f17c2e5ab85acdc3e82a07f9a741
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-10-17 19:59:24 -0700

    update views

--- controllers/budgets.js
+++ controllers/budgets.js
@@ -35,36 +35,36 @@ router.get("/new", (req, res) => {
 });
 // });
 
-//  create
+ //create
 
-// router.post("/", (req, res) => {
-//     const budget = new Budget(req.body);
-//     budget.save();
-//     res.redirect("/budget");
-// });
+router.post("/", (req, res) => {
+    const budget = new Budget(req.body);
+    budget.save();
+    res.redirect("/budget/" + budget._id);
+});
 
 //create
 
-router.post("/", (req, res) => {
-    const currentUser = req.user;
-    if (currentUser === null) {
-        return res.redirect("/user/login");
-    }
-    const post = new Budget(req.body);
-    budget.author = req.user._id;
-    budget.save().then(budget => {
-            return User.findById(req.user._id);
-        })
-        .then(user => {
-            user.budgets.unshift(budget);
-            user.save();
-            // REDIRECT TO THE NEW budget
-            res.redirect("/budget/" + budget._id);
-        })
-        .catch(err => {
-            console.log(err.message);
-        });
-});
+// router.post("/", (req, res) => {
+//     const currentUser = req.user;
+//     if (currentUser === null) {
+//         return res.redirect("/user/login");
+//     }
+//     const post = new Budget(req.body);
+//     budget.author = req.user._id;
+//     budget.save().then(budget => {
+//             return User.findById(req.user._id);
+//         })
+//         .then(user => {
+//             user.budgets.unshift(budget);
+//             user.save();
+//             // REDIRECT TO THE NEW budget
+//             res.redirect("/budget/" + budget._id);
+//         })
+//         .catch(err => {
+//             console.log(err.message);
+//         });
+// });
 
 // show
 router.get("/:id", (req, res) => {
